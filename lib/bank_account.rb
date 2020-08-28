31b31896a347730ffa0b77cba050efed57aae524
laflamablanc <sean.laflam@gmail.com>
2020-08-28 11:37:37 -0400

    All tests passing

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,37 @@
 class BankAccount
+  attr_reader :name
+  attr_accessor :status, :balance
 
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    @@all << self
+  end
+
+  def self.all
+    @@all    
+  end
+
+  def deposit(amount)
+    @balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    if self.balance > 0 && self.status == "open"
+      return true
+    else
+      return false
+    end
+  end
+
+  def close_account
+    @status = "closed"
+  end
 end
